a67dde72eeac7855efbac03318ebbbfd1fd8bfe6
Kőszegi Patrik <koszegipatrik888@gmail.com>
2025-03-28 00:28:24 +0100

    Added new demo code, interference happens with the clock ic.

--- demo-code/demo-code.ino
+++ demo-code/demo-code.ino
@@ -1,3 +1,5 @@
+#include <PCF8563Clock.h>
+
 const byte N_OF_SEGMENTS = 4;
 const byte SCREEN_COUNT = 5;
 
@@ -10,18 +12,17 @@ const byte dataPins[7] = { 7, 8, 9, 10, 11, 12, 13 };
 // Write pins for each display
 const byte writePins[SCREEN_COUNT] = { 2, 3, 4, 5, 6 };
 
-const String text = " 14:00  2025 03  27 ";
+String text = "";
 String splittedText[SCREEN_COUNT];
 
-void clearDisplays() {
-  digitalWrite(CLR, LOW);
-  delayMicroseconds(100);
-  digitalWrite(CLR, HIGH);
-  delayMicroseconds(100);
-}
+PCF8563Clock rtc;
 
 void setup() {
   Serial.begin(9600);
+  rtc.begin();
+
+  // Set the initial time
+  rtc.setTime(0, 56, 23, 28, 5, 3, 25); // 23:56:00 on March 28, 2024
 
   // Initialize pins
   pinMode(CLR, OUTPUT);
@@ -42,19 +43,24 @@ void setup() {
 
   // Clear displays
   clearDisplays();
+}
 
-  // Split text safely
-  int count = 0;
-  for (int i = 0; i < text.length() && count < SCREEN_COUNT; i += N_OF_SEGMENTS) {
-    splittedText[count] = text.substring(i, min(i + N_OF_SEGMENTS, text.length()));
+void loop() {
+  // Update time and text every second
+    updateTime();
 
-    // Pad with spaces if needed
-    while (splittedText[count].length() < N_OF_SEGMENTS) {
-      splittedText[count] = " " + splittedText[count];
+    // Split text safely
+    int count = 0;
+    for (int i = 0; i < text.length() && count < SCREEN_COUNT; i += N_OF_SEGMENTS) {
+      splittedText[count] = text.substring(i, min(i + N_OF_SEGMENTS, text.length()));
+
+      // Pad with spaces if needed
+      while (splittedText[count].length() < N_OF_SEGMENTS) {
+        splittedText[count] = " " + splittedText[count];
+      }
+
+      count++;
     }
-
-    count++;
-  }
 
   // Iterate through each display
   for (int screenIndex = 0; screenIndex < SCREEN_COUNT; screenIndex++) {
@@ -68,9 +74,6 @@ void setup() {
       displayChar(currentChar, writePins[screenIndex]);
     }
   }
-}
-
-void loop() {
 }
 
 void selectAddr(byte segment) {
@@ -94,7 +97,6 @@ void selectAddr(byte segment) {
   }
 }
 
-
 byte asciiToDL3416(char c) {
   // Convert character to display-compatible byte
   return (c >= ' ' && c <= '_') ? c : 0x20;
@@ -114,4 +116,19 @@ void displayChar(char c, int wrPin) {
   digitalWrite(wrPin, LOW);
   delayMicroseconds(10);
   digitalWrite(wrPin, HIGH);
+}
+
+void clearDisplays() {
+  digitalWrite(CLR, LOW);
+  delayMicroseconds(100);
+  digitalWrite(CLR, HIGH);
+  delayMicroseconds(100);
+}
+
+void updateTime() {
+  // Get the current time directly from RTC each time
+  text = rtc.getFormattedTime() + " " + rtc.getFormattedDate() + "  ";
+  
+  // Debug print to show time is updating
+  Serial.println("Current Time: " + text);
 }
